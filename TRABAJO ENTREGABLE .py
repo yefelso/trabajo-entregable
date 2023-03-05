864038c87acc265f1384a913847bcd322cf023ad
yefelso <1313124@senati.pe>
2023-03-05 00:02:06 -0500

    Trabajo Entregable

--- TRABAJO ENTREGABLE .py
+++ TRABAJO ENTREGABLE .py
@@ -1,4 +1,12 @@
-from tkinter import *
- 
-root = Tk()
-root.title('Ferretería El Tornillo Feliz')
+from _tkinter import *
+rot = TK()
+rot.title("FERRETERIA EL TORNILLO FELIZ")
+
+my Frame = Frame(rot)
+mi Frame.pack()
+# label y entri DNI
+obtenerdni = StringVar()
+iDni=label(myFrame,text="DNI:")
+idni.grit(row=0,column=0,stiki="e",pady=5,padx=5)
+tDni = Entry(my frame,text variable=obtener Dni)
+tDni .grit(row=0,column=1,pady=5,padx=5)
